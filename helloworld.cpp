16b2bbd8f1abd689394c8a298fc83b3187751c22
Isaac Joffe <isaac11joffe@gmail.com>
2022-01-12 19:52:49 -0700

    initial commit

---
+++ helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>    // for cout object
+using namespace std;    // eliminate use of std:: prefix
+
+// prints the string "Hello World!" to standard out
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;    // default return
+}
